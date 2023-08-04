ac23100ffa1a6ca38026129ed8a22cf558a3e5ad
Brendan Dodd <brendananthonydodd@gmail.com>
2023-08-04 23:29:30 +0200

    fixed navbar transition

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 window.addEventListener("scroll", function() {
-  var name = document.getElementById('name');
+  var navbar = document.querySelector('.navbar'); // adjust to your HTML structure
   var anthony = document.getElementById('anthony');
   var dodd = document.getElementById('dodd');
 
   if (window.scrollY > 0) { // Change the value based on when you want the effect to occur
     anthony.classList.add('hidden');
     dodd.classList.add('hidden');
+    navbar.classList.add('small-navbar'); // Add the small-navbar class
   } else {
     anthony.classList.remove('hidden');
     dodd.classList.remove('hidden');
+    navbar.classList.remove('small-navbar'); // Remove the small-navbar class
   }
 });
