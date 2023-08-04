284996f487e616bb2071e92a85ee474eb61d0f74
Brendan Dodd <brendananthonydodd@gmail.com>
2023-08-04 20:19:49 +0200

    updated navbar and home page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+window.addEventListener("scroll", function() {
+  var name = document.getElementById('name');
+  var anthony = document.getElementById('anthony');
+  var dodd = document.getElementById('dodd');
 
+  if (window.scrollY > 0) { // Change the value based on when you want the effect to occur
+    anthony.classList.add('hidden');
+    dodd.classList.add('hidden');
+  } else {
+    anthony.classList.remove('hidden');
+    dodd.classList.remove('hidden');
+  }
+});
