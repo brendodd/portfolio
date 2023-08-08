19a868b4833d6f39b2d9f0eb698bebe222413c43
Brendan Dodd <brendananthonydodd@gmail.com>
2023-08-08 17:53:39 +0200

    edited the index pages

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
 window.addEventListener("scroll", function() {
-  var navbar = document.querySelector('.navbar'); // adjust to your HTML structure
+  var navbar = document.querySelector('.navbar');
   var anthony = document.getElementById('anthony');
   var dodd = document.getElementById('dodd');
 
-  if (window.innerWidth > 1024) { // Check the viewport width
-    if (window.scrollY > 0) { // Change the value based on when you want the effect to occur
+  if (window.innerWidth > 1024) {
+    navbar.style.position = 'sticky'; // Ensure the navbar is sticky
+
+    if (window.scrollY > 0) {
       anthony.classList.add('hidden');
       dodd.classList.add('hidden');
-      navbar.classList.add('small-navbar'); // Add the small-navbar class
+      navbar.classList.add('small-navbar');
     } else {
       anthony.classList.remove('hidden');
       dodd.classList.remove('hidden');
-      navbar.classList.remove('small-navbar'); // Remove the small-navbar class
+      navbar.classList.remove('small-navbar');
     }
+  } else {
+    navbar.style.position = 'relative'; // Make navbar non-sticky for widths less than 1024px
   }
+});
+
+const backToTopButton = document.getElementById('back-to-top');
+
+backToTopButton.addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
